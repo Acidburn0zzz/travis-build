13002b56b288e887e1594716997b07e4b90ff9b2
Hiro Asari <asari.ruby@gmail.com>
2015-09-09 12:44:11 -0400

    Start work on MySQL addon

---
+++ lib/travis/build/addons/mysql.rb
@@ -0,0 +1,41 @@
+require 'shellwords'
+require 'travis/build/addons/base'
+
+module Travis
+  module Build
+    class Addons
+      class Mysql < Base
+        SUPER_USER_SAFE = true
+
+        MARIADB_GPG_KEY = '0xcbcb082a1bb943db'
+        MARIADB_MIRROR  = 'nyc2.mirrors.digitalocean.com'
+
+        MYSQL_APT_CONFIG_VERSION = '0.3.5'
+
+        def after_prepare
+          sh.fold 'mysql' do
+            sh.echo "Installing MySQL version #{mysql_version}", ansi: :yellow
+            sh.cmd "service mysql stop", sudo: true
+            sh.cmd "wget http://dev.mysql.com/get/#{config_file}"
+            sh.cmd "dpkg -i #{config_file}", sudo: true
+            sh.cmd "apt-get update -qq", assert: false, sudo: true
+            sh.cmd "dpkg-reconfigure "
+            sh.cmd "apt-get install -o Dpkg::Options::='--force-confnew' mariadb-server", sudo: true, echo: true, timing: true
+            sh.echo "Starting MySQL v#{mysql_version}", ansi: :yellow
+            sh.cmd "service mysql start", sudo: true, assert: false, echo: true, timing: true
+            sh.cmd "mysql --version", assert: false, echo: true
+          end
+        end
+
+        private
+        def mysql_version
+          config.to_s.shellescape
+        end
+
+        def config_file
+          "mysql-apt-config_#{MYSQL_APT_CONFIG_VERSION}-1ubuntu$(lsb_release -cs)_all.deb"
+        end
+      end
+    end
+  end
+end
